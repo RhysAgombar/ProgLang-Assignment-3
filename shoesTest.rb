9b8585053a31411ddc611fc95080fafb0ad15531
Rhys Agombar <Rhys.Agombar@uoit.net>
2016-11-28 21:57:11 -0500

    Added Bubble sort and Selection sort and fixed some GUI issues

--- shoesTest.rb
+++ shoesTest.rb
@@ -3,6 +3,45 @@ def genStrings
   return (0...3).map{ charset.to_a[rand(charset.size)] }.join
 end
 
+def bSort(list)
+  swapped = true
+  
+  while swapped == true
+    swapped = false
+    for pos in 0 ... (list.size - 1)
+      for i in 0 ... list.size
+        if ((i == pos) || (i == (pos + 1)))
+          $t = para list[i], :font => "Monospace 12px", :stroke => red
+        else
+          $t = para list[i], :font => "Monospace 12px", :stroke => black
+        end    
+      end
+      
+      $t = para "\n", :font => "Monospace 12px", :stroke => black
+      
+      if (list.at(pos) > list.at(pos + 1))
+        swapped = true        
+        list[pos], list[pos+1] = list[pos+1], list[pos]
+      end
+    end
+  end 
+  
+end
+
+def bubble_sort(list)
+  result = bSort(list)
+  
+  $t = para "Bubble Sort Complete", :font => "Monospace 12px", :stroke => black
+  $t = para "\n", :font => "Monospace 12px", :stroke => black
+
+  for i in 0 ... $uList.size
+      $t = para $uList[i], :font => "Monospace 12px", :stroke => black
+  end
+  
+  return
+end
+
+
 def qSort(list, start, stop, pivotPos) 
   pivot = list.size - 1
   left = []
@@ -87,41 +126,145 @@ def quick_sort(list, start, stop, pivotPos)
   for i in 0 ... $uList.size
       $t = para $uList[i], :font => "Monospace 12px", :stroke => black
   end
+  
+  return
 
 end
 
-Shoes.app :width => 700, :height => 415 do
+def sSort(list)
+  holderPos = 0
+    for pos in 0 ... list.size
+      holderPos = pos
+      for pos2 in pos ... list.size
+        if (list.at(pos2) < list.at(pos))          
+          if (list.at(pos2) < list.at(holderPos))
+            holderPos = pos2
+          end
+        end
+      end        
+      
+      $t = para "\n", :font => "Monospace 12px", :stroke => black
+      
+      for i in 0 ... list.size
+        if ((i == pos) || (i == holderPos))
+          $t = para list[i], :font => "Monospace 12px", :stroke => red
+        else
+          $t = para list[i], :font => "Monospace 12px", :stroke => black
+        end    
+      end
+      
+      list[pos], list[holderPos] = list[holderPos], list[pos]          
+    end
+        
+    return list    
+end
+
+def selection_sort(list)
+  result = sSort(list)  
+  
+  $t = para "\n", :font => "Monospace 12px", :stroke => black
+  $t = para "Selection Sort Complete", :font => "Monospace 12px", :stroke => black, :displace_left => 0
+  $t = para "\n", :font => "Monospace 12px", :stroke => black
+
+  for i in 0 ... $uList.size
+      $t = para $uList[i], :font => "Monospace 12px", :stroke => black
+  end
+  
+end
+
+def bButtonClick  
+  clear()
+  
+  genButtons
+  
+  #$t = para "\n", :font => "Monospace 1px", underline: "single"
+  
+  $uList = (0...4).map{ genStrings }
+    
+  for i in 0 ... $uList.size
+      $t = para $uList[i], :font => "Monospace 12px", :stroke => black    
+  end
+  
+  $t = para "\n", :font => "Monospace 12px", :stroke => black
+        
+  lst = $uList
+      
+  bubble_sort(lst)
+  
+  return 
+end
+
+
+def sButtonClick  
+  clear()
+  
+  genButtons
+  
+  #$t = para "\n", :font => "Monospace 1px", underline: "single"
+  
+  $uList = (0...6).map{ genStrings }
+    
+  for i in 0 ... $uList.size
+      $t = para $uList[i], :font => "Monospace 12px", :stroke => black    
+  end
+  
+  #$t = para "\n", :font => "Monospace 12px", :stroke => black
+        
+  lst = $uList
+      
+  selection_sort(lst)
+  
+  return 
+end
+
+def qButtonClick  
+  clear()
+  
+  genButtons
+  
+  #$t = para "\n", :font => "Monospace 1px", underline: "single"
+  
+  $uList = (0...6).map{ genStrings }
+    
+  for i in 0 ... $uList.size
+    if (i == $uList.size - 1)
+      $t = para $uList[i], :font => "Monospace 12px", underline: "single"
+    else
+      $t = para $uList[i], :font => "Monospace 12px", :stroke => black
+    end    
+  end
+  
+  $t = para "\n", :font => "Monospace 12px", :stroke => black
+        
+  lst = $uList
+      
+  quick_sort(lst, 0, (lst.size-1), (lst.size-1))
+  
+  return 
+end
+
+def genButtons
+  button "Quick Sort", :displace_top => 385 do
+    qButtonClick
+  end
+  
+  button "Selection Sort", :displace_top => 385, :displace_left => 0 do
+    sButtonClick
+  end
+  
+  button "Bubble Sort", :displace_top => 385, :displace_left => 0 do
+    bButtonClick
+  end
+end
+
+Shoes.app :width => 376, :height => 415 do
   background "../static/menu-gray.png"
   background "../static/menu-top.png", :height => 50
   background "../static/menu-left.png", :top => 50, :width => 55
   background "../static/menu-right.png", :right => 0, :top => 50, :width => 55
   image "../static/menu-corner1.png", :top => 0, :left => 0
   image "../static/menu-corner2.png", :right => 0, :top => 0
-
-  ##$disp = edit_box width: 700, height: 385
-
-  #$t = para "", :font => "Monospace 12px", :stroke => black
-
-  #button "Quick Sort" do
-    #$disp = ""
     
-    $uList = (0...6).map{ genStrings }
-    
-    for i in 0 ... $uList.size
-      if (i == $uList.size - 1)
-        $t = para $uList[i], :font => "Monospace 12px", underline: "single"
-      else
-        $t = para $uList[i], :font => "Monospace 12px", :stroke => black
-      end    
-    end
-    
-    $t = para "\n", :font => "Monospace 12px", :stroke => black
-      
-    lst = $uList
-    
-    quick_sort(lst, 0, (lst.size-1), (lst.size-1))
-    
-    
-  #end
-
+  qButtonClick()
+  
 end
